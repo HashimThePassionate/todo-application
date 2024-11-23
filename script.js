a456e92eb0c31db0a868d0e32832c3784f75f150
HashimThePassionate <hashiimtahir@gmail.com>
2024-11-23 23:18:07 +0500

    Script Code

--- script.js
+++ script.js
@@ -1,56 +1,60 @@
 let id = (id) => document.getElementById(id);
-let btn = id('add-todo-btn')
 
-let addtodobtn = id('add-todo-btn')
+let addtodobtn = id('add-todo-btn');
 let todopopup = id('todo-popup');
 let savetodo = id('save-todo-btn');
 let closepopupbtn = id('close-popup-btn');
 let todoinput = id('todo-input');
 let todolist = id('todo-list');
 
+let deletePopup = id('delete-popup');
+let confirmDeleteBtn = id('confirm-delete-btn');
+let cancelDeleteBtn = id('cancel-delete-btn');
+let deleteIndex = null;
+
+let alertPopup = id('alert-popup');
+let closeAlertBtn = id('close-alert-btn');
+
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
 let editindex = null;
 
-addtodobtn.addEventListener('click',()=>{
+addtodobtn.addEventListener('click', () => {
     todopopup.style.display = 'flex';
     todoinput.value = '';
     editindex = null;
 });
 
-closepopupbtn.addEventListener(
-    'click', () => {
-        todopopup.style.display = 'none';
-    }
-);
+closepopupbtn.addEventListener('click', () => {
+    todopopup.style.display = 'none';
+});
 
-savetodo.addEventListener(
-    'click', 
-    () => {
-        let todotext = todoinput.value.trim();
-        if (todotext){
-            if (editindex !== null){
-                todos[editindex] = todotext
-            }
-            else{
-                todos.push(todotext)
-            }
-        localStorage.setItem('todos',
-            JSON.stringify(todos)
-        )
+savetodo.addEventListener('click', () => {
+    let todotext = todoinput.value.trim();
+    if (todotext) {
+        if (editindex !== null) {
+            todos[editindex] = todotext;
+        } else {
+            todos.push(todotext);
+        }
+        localStorage.setItem('todos', JSON.stringify(todos));
         renderTodos();
         todopopup.style.display = 'none';
-        }
-        else{
-            alert('Please enter a todo')
-        }
+    } else {
+        // Show the custom alert popup
+        alertPopup.style.display = 'flex';
     }
-);
+});
+
+// Close the alert popup
+closeAlertBtn.addEventListener('click', () => {
+    alertPopup.style.display = 'none';
+});
 
 // Render Todos
 function renderTodos() {
-    todolist.innerHTML = ''; // Clear the existing list
+    todolist.innerHTML = '';
     todos.forEach((todo, index) => {
-        let li = document.createElement('li'); // Create a new list item
+        let li = document.createElement('li');
         li.innerHTML = `
             <span>${todo}</span>
             <div>
@@ -58,23 +62,35 @@ function renderTodos() {
                 <button class="delete-btn" onclick="deleteTodo(${index})">Delete</button>
             </div>
         `;
-        todolist.appendChild(li); // Add the list item to the todo list
+        todolist.appendChild(li);
     });
 }
 
-window.editTodo = function (i){
+window.editTodo = function (i) {
     todoinput.value = todos[i];
     editindex = i;
     todopopup.style.display = 'flex';
-}
+};
 
 window.deleteTodo = function (index) {
-    if (confirm('Are you sure you want to delete this todo?')) {
-        todos.splice(index, 1);
+    deleteIndex = index;
+    deletePopup.style.display = 'flex';
+};
+
+confirmDeleteBtn.addEventListener('click', () => {
+    if (deleteIndex !== null) {
+        todos.splice(deleteIndex, 1);
         localStorage.setItem('todos', JSON.stringify(todos));
         renderTodos();
+        deletePopup.style.display = 'none';
+        deleteIndex = null;
     }
-};
+});
+
+cancelDeleteBtn.addEventListener('click', () => {
+    deletePopup.style.display = 'none';
+    deleteIndex = null;
+});
 
 // Initial Load
 renderTodos();
